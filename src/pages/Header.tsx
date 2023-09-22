3d5c95dd2cea127b4a8291c8bf48488dd0cafc72
DrStone <drstoneluffy@gmail.com>
2023-09-23 01:00:43 +0500

    Added Header

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -26,7 +26,7 @@ const Header = (props: Props) => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 z-50"
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-50"
           >
             <li>
               <a>Item 1</a>
